ddd862344b38c2ad26586995cf3382347d208435
MSobrecasas <sns.mary@hotmail.com>
2023-01-13 18:26:56 -0300

    Agregar archivos de proyecto.

---
+++ LibErrores/Medico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibErrores
+{
+    public class Medico
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if (value.Trim().Length < 1)
+                {
+                    throw new Exception("El nombre no puede ser menor que 1");
+                }
+                if (value.Trim().Length > 50)
+                {
+                    throw new Exception("El nombre no puede ser mayor que 50");
+                }
+                nombre = value.Trim();
+            }
+        }
+
+        private string apellido;
+        public string Apellido { get; set; }
+
+        private int matricula;
+        public int Matricula { get; set; }
+    }
+}
---
+++ WindowsFormsErrores/Form1.cs
@@ -0,0 +1,66 @@
+﻿using LibErrores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsErrores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            MenorUno();
+            MayorCincuenta();
+            NombreValido();
+        }
+
+        private static void MenorUno()
+        {
+            try
+            {
+                Medico medico = new Medico() { Nombre = "", Apellido = "", Matricula = 123 };
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error menor de 1");
+            }
+        }
+
+        private static void MayorCincuenta()
+        {
+            try
+            {
+                Medico medico = new Medico() { Nombre = "111111111111111111111111111111111111111111111111111", Apellido = "", Matricula = 123 };
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error mayor de 50");
+            }
+        }
+        private static void NombreValido()
+        {
+            try
+            {
+                Medico medico = new Medico() { Nombre = " Juan  ", Apellido = "Perez", Matricula = 123 };
+                MessageBox.Show("Nombre: " + medico.Nombre + medico.Apellido + " Matricula: " + medico.Matricula, "Creado Correctamente");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
